999a382bc87afb7f8d5ce114dd25b8b8657366a3
sarisgar28 <sarisgar28@gmail.com>
2021-01-03 18:00:56 -0500

    commit tree 293298a5e4e8d089fa01b4150436755c4dca4161

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+function updateDriverWithKeyAndValue(driver, object, key, value){
+ const newDriver = {driver, object};
+
+ newDriver[key] = value;
+
+ return newDriver
+}
